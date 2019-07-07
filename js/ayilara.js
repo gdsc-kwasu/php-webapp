db6505665d64fb2cf84b02ae42dee4bfb99ae646
Adminixtrator <olamilekan.adekunle@gamil.com>
2019-07-07 08:28:28 -0700

    re-arrangement

--- js/ayilara.js
+++ js/ayilara.js
@@ -24,29 +24,3 @@ function myFunction() {
         moreText.style.display = "inline";
     }
 }
-function myfunction() {
-
-    var dots =
-
-    document.getElementById("dots");
-    
-    var moreText =
-
-    document.getElementById("more1");
-
-    var btnText = 
-
-    document.getElementById("read_more");
-
-    if (dots.style.display === "none"){
-
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    }
-    else{
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
-    }
-}
