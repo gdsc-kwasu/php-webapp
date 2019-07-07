c8287c432538c82ac18957d3301c06bdf52ccac9
Adminixtrator <olamilekan.adekunle@gamil.com>
2019-07-07 02:22:41 -0700

    made changes

---
+++ js/ayilara.js
@@ -0,0 +1,52 @@
+function myFunction() {
+
+    var dots =
+
+    document.getElementById("dots");
+    
+    var moreText =
+
+    document.getElementById("more");
+
+    var btnText = 
+
+    document.getElementById("myBtn");
+
+    if (dots.style.display === "none"){
+
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+function myfunction() {
+
+    var dots =
+
+    document.getElementById("dots");
+    
+    var moreText =
+
+    document.getElementById("more1");
+
+    var btnText = 
+
+    document.getElementById("read_more");
+
+    if (dots.style.display === "none"){
+
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
